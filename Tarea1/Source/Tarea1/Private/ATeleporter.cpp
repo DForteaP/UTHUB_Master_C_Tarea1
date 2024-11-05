86529e667254b794b4e19f1521b63eef3ce59e58
DForteaP <dforteap@gmail.com>
2024-11-05 19:59:44 +0100

    Teleport working

---
+++ Tarea1/Source/Tarea1/Private/ATeleporter.cpp
@@ -0,0 +1,70 @@
+﻿#include "Tarea1/Public/ATeleporter.h"
+#include "Components/BoxComponent.h"
+
+AATeleporter::AATeleporter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bIsTeleporting = false;
+	TeleportArea = CreateDefaultSubobject<UBoxComponent>(TEXT("TeleportArea"));
+	RootComponent = TeleportArea;
+	
+	TeleportArea->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	TeleportArea->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
+	TeleportArea->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	
+	TeleportArea->OnComponentBeginOverlap.AddDynamic(this, &AATeleporter::OnOverlapBegin);
+	TeleportArea->OnComponentEndOverlap.AddDynamic(this, &AATeleporter::OnOverlapEnd);
+}
+
+void AATeleporter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AATeleporter::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (bIsTeleporting) return;
+	if (DestinationTeleporter && OtherActor && OtherActor->IsA(ActorTypeToTeleport))
+	{
+		DestinationTeleporter->bIsTeleporting = true;
+		FVector DestinationLocation = DestinationTeleporter->GetActorLocation();
+		OtherActor->SetActorLocation(DestinationLocation);
+	}
+}
+
+void AATeleporter::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor && OtherActor->IsA(ActorTypeToTeleport))
+	{
+		bIsTeleporting = false;
+	}
+}
+
+void AATeleporter::SetDestinationTeleporter(AATeleporter* NewDestination)
+{
+	DestinationTeleporter = NewDestination;
+	if (NewDestination && NewDestination->DestinationTeleporter != this)
+	{
+		NewDestination->SetDestinationTeleporter(this);
+	}
+}
+
+#if WITH_EDITOR
+	void AATeleporter::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+	{
+		Super::PostEditChangeProperty(PropertyChangedEvent);
+
+		FName PropertyName;
+		if (PropertyChangedEvent.Property)
+		{
+			PropertyName = PropertyChangedEvent.Property->GetFName();
+		} else{
+			PropertyName = NAME_None;
+		}
+		
+		if (PropertyName == GET_MEMBER_NAME_CHECKED(AATeleporter, DestinationTeleporter))
+		{
+			SetDestinationTeleporter(DestinationTeleporter);
+		}
+	}
+#endif
---
+++ Tarea1/Source/Tarea1/Public/ATeleporter.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ATeleporter.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class TAREA1_API AATeleporter : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AATeleporter();
+
+	//CONTROLA SI SE MODIFICA LA PROPIEDAD EN EL EDITOR
+	#if WITH_EDITOR
+		virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	#endif
+	
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Teleporter")
+	UBoxComponent* TeleportArea;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleporter")
+	AATeleporter* DestinationTeleporter;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleporter")
+	TSubclassOf<AActor> ActorTypeToTeleport;
+
+	//EVENTOS DE CONTROL DE COLISION
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:
+	void SetDestinationTeleporter(AATeleporter* NewDestination);
+	bool bIsTeleporting;
+};
+
