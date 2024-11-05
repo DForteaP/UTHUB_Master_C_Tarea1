1a0c06ce1f6e7c33c3304213c54d3449fb725ac5
DForteaP <dforteap@gmail.com>
2024-11-05 14:39:52 +0100

    ConvertedTo5.4

---
+++ Tarea1/Source/Tarea1/Tarea1.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Tarea1 : ModuleRules
+{
+	public Tarea1(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Tarea1/Source/Tarea1/Tarea1.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tarea1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tarea1, "Tarea1" );
+ 
